a041cc131d2adf965a48cd34f4112e970515a57f
Cameron Cahill <cameron.cahill@valpo.edu>
2026-04-30 21:03:41 +0200

    Add compatibility wrapper for testbench

--- src/project.v
+++ src/project.v
@@ -24,3 +24,27 @@ module tt_um_camcahill (
     wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
+// Compatibility wrapper for the default testbench
+module tt_um_example (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
+
+    tt_um_camcahill user_project (
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+        .uio_in(uio_in),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe),
+        .ena(ena),
+        .clk(clk),
+        .rst_n(rst_n)
+    );
+
+endmodule
